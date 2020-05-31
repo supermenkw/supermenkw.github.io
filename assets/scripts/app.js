1c545140472b42d773adeb7de9105eb985859318
Angga M. Ginanjar <36956295+supermenkw@users.noreply.github.com>
2020-05-31 21:14:30 +0700

    initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,95 @@
+particlesJS('particles-js',
+
+    {
+        "particles": {
+            "number": {
+                "value": 20,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#4299e1"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.30246529723245905,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 20,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": false,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "bottom",
+                "random": false,
+                "straight": false,
+                "out_mode": "bounce",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "window",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                },
+                "onclick": {
+                    "enable": false,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "repulse": {
+                    "distance": 100,
+                    "duration": 0.5
+                }
+            }
+        },
+        "retina_detect": true
+    }
+
+);
---
+++ assets/scripts/index.js
@@ -0,0 +1,15 @@
+const writingText = document.querySelector("#writing-text");
+
+Typed.new('#writing-text', {
+    strings: [
+        'I am a Full-stack Web Developer!^1000',
+        'i love to try and learn new things.^1000',
+        'especially frontend.^1000',
+        'happy coding!'
+    ],
+    typeSpeed: 40,
+    callback: function () {
+        writingText.style.color = '#ffff';
+        writingText.style.background = '#4299e1';
+    },
+})
