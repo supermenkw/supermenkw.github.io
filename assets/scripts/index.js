b818d31fdcd3e4694b935c42b30189e052cbcc83
Angga M. Ginanjar <36956295+supermenkw@users.noreply.github.com>
2020-06-09 20:55:02 +0700

    redesign to bussuiness card style.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,15 +1,32 @@
-const writingText = document.querySelector("#writing-text");
-
-Typed.new('#writing-text', {
-    strings: [
-        'I am a Full-stack Web Developer!^1000',
-        'i love to try and learn new things.^1000',
-        'especially frontend.^1000',
-        'happy coding!'
-    ],
-    typeSpeed: 40,
-    callback: function () {
-        writingText.style.color = '#ffff';
-        writingText.style.background = '#4299e1';
+new Vue({
+    el: "#app",
+    data: {
+        open: false
     },
-})
+    methods: {
+        openCard: function () {
+            this.open = !this.open;
+        }
+    }
+});
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
